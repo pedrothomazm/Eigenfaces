1728e72f41d525ddf672924e85cef7fbeae5ffbf
Pedro Thomaz Conzatti Martins <pedrothomazm@gmail.com>
2022-11-21 18:48:44 -0300

    Create read_dataset function

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+from typing import Iterable
 import numpy as np
+import glob
 from PIL import Image
 
 # Dimensions that the images will be resized to for analysis
@@ -24,3 +26,19 @@ def read_image(path: str) -> np.ndarray:
     # Converts to an 1D array and normalizes it
     image = image.flatten() / 255
     return image
+
+
+def read_dataset(path: str, extension: str) -> Iterable[np.ndarray]:
+    """Reads all images of the specified format in the specified folder
+
+    :param path: Path to the folder
+    :type path: str
+    :param extension: Extension of the images being read
+    :type extension: str
+    :return: Iterable containing the images converted to ndarrays using read_image
+    :rtype: Iterable[numpy.ndarray]
+    """
+    pathname_pattern = f"{path}/*.{extension}"
+    path_list = glob.glob(pathname_pattern)
+    map_iter = map(read_image, path_list)
+    return map_iter
